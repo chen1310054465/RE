8e5141913abee18a6734a87630064404b55a70cd
dtera <huster1005@gmail.com>
2019-01-06 21:41:17 +0800

    refactor project

--- draw_plot.py
+++ draw_plot.py
@@ -18,13 +18,15 @@ result_dir = os.path.join('test_result', FLAGS.dn)
 
 def main():
     models = sys.argv[1:]
-    for model in models:
+    # markers = ['D', '*', 's', 'p', 'o', '^', '+', 'x']
+    for i, model in enumerate(models):
         x = np.load(os.path.join(result_dir, model + '_x' + '.npy'))
         y = np.load(os.path.join(result_dir, model + '_y' + '.npy'))
         f1 = (2 * x * y / (x + y + 1e-20)).max()
         auc = sklearn.metrics.auc(x=x, y=y)
         # plt.plot(x, y, lw=2, label=model + '-auc='+str(auc))
         plt.plot(x, y, lw=2, label=model)
+        # plt.plot(x[::10000], y[::10000], markers[i], label=model)
         print(model + ' : ' + 'auc = ' + str(auc) + ' | ' + 'max F1 = ' + str(f1))
         print('    P@100: {} | P@200: {} | P@300: {} | Mean: {}'.format(y[100], y[200], y[300],
                                                                         (y[100] + y[200] + y[300]) / 3))
@@ -34,7 +36,8 @@ def main():
     plt.ylim([0.3, 1.0])
     plt.xlim([0.0, 0.4])
     plt.title('Precision-Recall')
-    plt.legend(loc="upper right", fontsize='small')
+    # plt.legend(loc="upper right", fontsize='small')
+    plt.legend(loc="upper right")
     plt.grid(True)
     plt.savefig(os.path.join(result_dir, FLAGS.dn + '_pr_curve'))
 
