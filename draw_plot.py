fc9dca3889abdb1d1c17a5bde5ebfd7f9e92ad16
dtera <huster1005@gmail.com>
2019-01-07 13:37:05 +0800

    refactor project

--- draw_plot.py
+++ draw_plot.py
@@ -18,14 +18,15 @@ result_dir = os.path.join('test_result', FLAGS.dn)
 
 def main():
     models = sys.argv[1:]
-    # markers = ['D', '*', 's', 'p', 'o', '^', '+', 'x']
+    # fmt = ['rD-', 'g*--', 'bs-.', 'yp:', 'mo--', 'c^--', 'k+--', 'kx--']
+    ls = ['-', '--', ':', '-.', '--', '--', '--', '--']
     for i, model in enumerate(models):
         x = np.load(os.path.join(result_dir, model + '_x' + '.npy'))
         y = np.load(os.path.join(result_dir, model + '_y' + '.npy'))
         f1 = (2 * x * y / (x + y + 1e-20)).max()
         auc = sklearn.metrics.auc(x=x, y=y)
         # plt.plot(x, y, lw=2, label=model + '-auc='+str(auc))
-        plt.plot(x, y, lw=2, label=model)
+        plt.plot(x, y, ls[i], lw=2, label=model)
         # plt.plot(x[::10000], y[::10000], markers[i], label=model)
         print(model + ' : ' + 'auc = ' + str(auc) + ' | ' + 'max F1 = ' + str(f1))
         print('    P@100: {} | P@200: {} | P@300: {} | Mean: {}'.format(y[100], y[200], y[300],
