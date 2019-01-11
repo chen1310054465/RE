348ee742747a0f5873b8211721610342ec810378
dtera <huster1005@gmail.com>
2019-01-11 16:23:10 +0800

    refactor project

--- draw_plot.py
+++ draw_plot.py
@@ -19,15 +19,20 @@ result_dir = os.path.join('test_result', FLAGS.dn)
 def main():
     models = sys.argv[1:]
     # fmt = ['rD-', 'g*--', 'bs-.', 'yp:', 'mo--', 'c^--', 'k+--', 'kx--']
+    cs = {'nyt_ete_rprn_att_ad': 'b', 'nyt_ete_rprn_att': 'g', 'nyt_ete_pcnn_att_ad': 'r', 'nyt_ete_pcnn_att': 'c',
+          'nyt_rprn_att_ad': 'm', 'nyt_rprn_att': 'y'}
     ls = ['-', '--', ':', '-.', '--', '--', '--', '--']
+
     for i, model in enumerate(models):
         x = np.load(os.path.join(result_dir, model + '_x' + '.npy'))
         y = np.load(os.path.join(result_dir, model + '_y' + '.npy'))
         f1 = (2 * x * y / (x + y + 1e-20)).max()
         auc = sklearn.metrics.auc(x=x, y=y)
         # plt.plot(x, y, lw=2, label=model + '-auc='+str(auc))
-        plt.plot(x, y, ls[i], lw=2, label=model)
-        # plt.plot(x[::10000], y[::10000], markers[i], label=model)
+        c = 'k'
+        if model in cs:
+            c = cs[model]
+        plt.plot(x, y, c + ls[i], lw=2, label=model)
         print(model + ' : ' + 'auc = ' + str(auc) + ' | ' + 'max F1 = ' + str(f1))
         print('    P@100: {} | P@200: {} | P@300: {} | Mean: {}'.format(y[100], y[200], y[300],
                                                                         (y[100] + y[200] + y[300]) / 3))
